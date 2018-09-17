1437e487749ac78c7d14f951b9df75b472f24981
cgfelipe <felipe.cavalcanti@hotmail.com>
2018-09-17 12:38:59 -0300

    adicionadas urls

--- tcc/urls.py
+++ tcc/urls.py
@@ -13,9 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
+from .. import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('/admin', admin.site.urls),
+    path('/index', views.index),
+    path('/cadastrar_estudante', views.cadastrar_estudante),
+    path('/listar_estudantes', views.listar_estudantes),
+    path('/cadastrar_professor', views.cadastrar_professor),
+    path('/listar_professores', views.listar_professores),
+    path('/cadastrar_departamento', views.cadastrar_departamento),
+    path('/listar_departamentos', views.listar_departamentos),
+    path('/cadastrar_instituicao', views.cadastrar_instituicao),
+    path('/listar_instituicoes', views.listar_instituicoes),
+    path('/cadastrar_livro', views.cadastrar_livro),
+    path('/listar_livros', views.listar_livros),
+    path('/cadastrar_curriculo', views.cadastrar_curriculo),
+    path('/listar_curriculos', views.listar_curriculos),
+    path('/cadastrar_projeto_pesquisa', views.cadastrar_projeto_pesquisa),
+    path('/listar_projetos_pesquisa', views.listar_projetos_pesquisa),
+    path('/cadastrar_especialidade', views.cadastrar_especialidade),
+    path('/listar_especialidades', views.listar_especialidades),
 ]
