597ca49a6e5fdf0cb3d7256a815bdc476942db2b
cgfelipe <felipe.cavalcanti@hotmail.com>
2018-11-10 14:52:54 -0300

    mais metodos para atualizacao e exclusao

--- core/views.py
+++ core/views.py
@@ -12,27 +12,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response
 import json
 
-# Create your views here.
-
 
 def index(request):
     return render(request, 'index.html')
-#
-# class EspecialidadeDelete(DeleteView):
-#     print(DeleteView)
-#     model = models.Especialidade.remover()
-#     success_url = reverse_lazy('lista_especialidades')
 
-
+#------------------ LISTA DE ITENS -----------------
 def artigos(request):
     lista = models.Artigo.listar_todos()
     return render(request, 'lista_artigos.html', {"lista": lista})
 
-
 def livros(request):
     lista = models.Livro.listar_todos()
     return render(request, 'lista_livros.html', {"lista": lista})
-
 
 def estudantes(request):
     lista = models.Estudante.listar_todos()
@@ -42,38 +33,32 @@ def professores(request):
     lista = models.Professor.listar_todos()
     return render(request, 'lista_professores.html', {"lista": lista})
 
-
 def curriculos(request):
     lista = models.Curriculo.listar_todos()
     return render(request, 'lista_curriculos.html', {"lista": lista})
-
 
 def instituicoes(request):
     lista = models.Instituicao.listar_todos()
     return render(request, 'lista_instituicoes.html', {"lista": lista})
 
-
 def projetos_pesquisa(request):
     lista = models.ProjetoDePesquisa.listar_todos()
     return render(request, 'lista_projetos_pesquisa.html', {"lista": lista})
-
 
 def especialidades(request):
     lista = models.Especialidade.listar_todos()
     return render(request, 'lista_especialidades.html', {"lista": lista})
 
-
 def departamentos(request):
     lista = models.Departamento.listar_todos()
     return render(request, 'lista_departamentos.html', {"lista": lista})
-
 
 def escolaridades(request):
     lista = models.Escolaridade.listar_todos()
     print(lista)
     return render(request, 'lista_escolaridades.html', {"lista": lista})
 
-
+#------------------ CADASTRO DE ITENS -----------------
 def cadastro_estudante(request):
     if request.method == 'POST':
         form = forms.EstudanteForm(request.POST)
@@ -96,7 +81,6 @@ def cadastro_estudante(request):
         form_pessoa = forms.PessoaForm()
         return render(request, 'cadastro_estudante.html', {'form': form, 'form_pessoa': form_pessoa})
 
-
 def cadastro_professor(request):
     if request.method == 'POST':
         form = forms.ProfessorForm(request.POST)
@@ -116,9 +100,6 @@ def cadastro_professor(request):
         form = forms.ProfessorForm()
         return render(request, 'cadastro_professor.html', {'form': form, 'form_endereco': form_endereco, 'form_pessoa': form_pessoa})
 
-
-
-
 def cadastro_instituicao(request):
     if request.method == 'POST':
         form = forms.InstituicaoForm(request.POST)
@@ -133,7 +114,6 @@ def cadastro_instituicao(request):
         form = forms.InstituicaoForm()
         form_endereco = forms.EnderecoForm()
         return render(request, 'cadastro_instituicao.html', {'form': form, 'form_endereco': form_endereco})
-
 
 def cadastro_curriculo(request):
     if request.method == 'POST':
@@ -179,7 +159,6 @@ def cadastro_escolaridade(request):
         form = forms.EspecialidadeForm()
     return render(request, 'cadastro_escolaridade.html', {'form': form})
 
-
 def cadastro_livro(request):
     if request.method == 'POST':
         form = forms.LivroForm(request.POST)
@@ -190,7 +169,6 @@ def cadastro_livro(request):
     else:
         form = forms.LivroForm()
         return render(request, 'cadastro_livro.html', {'form': form})
-
 
 def cadastro_projeto_pesquisa(request):
     if request.method == 'POST':
@@ -203,7 +181,6 @@ def cadastro_projeto_pesquisa(request):
         form = forms.ProjetoPesquisaForm()
         return render(request, 'cadastro_livro.html', {'form': form})
 
-
 def cadastro_departamento(request):
     if request.method == 'POST':
         form = forms.DepartamentoForm(request.POST)
@@ -215,7 +192,6 @@ def cadastro_departamento(request):
         form = forms.DepartamentoForm()
         return render(request, 'cadastro_departamento.html', {'form': form})
 
-
 def cadastro_especialidade(request, form=None):
     if request.method == 'POST':
         form = forms.EspecialidadeForm(request.POST)
@@ -226,7 +202,6 @@ def cadastro_especialidade(request, form=None):
     else:
         form = forms.EspecialidadeForm()
         return render(request, 'cadastro_especialidade.html', {'form': form})
-
 
 def cadastro_artigo(request):
     if request.method == 'POST':
@@ -244,12 +219,11 @@ def excluir_escolaridade(request):
         models.Escolaridade.remover()
         return redirect(request, 'lista_escolaridades.html')
 
-#------------------ AQUI FUNCIONANDO -----------------
+#------------------ EXCLUSAO DE ITENS -----------------
 def excluir_especialidade(request, id):
     e = get_object_or_404(models.Especialidade, id=id)
     e.delete()
     return especialidades(request)
-#------------------------------------------------------
 
 def excluir_estudante(request, id):
     e = get_object_or_404(models.Estudante, id=id)
@@ -271,7 +245,27 @@ def excluir_livro(request, id):
     e.delete()
     return livros(request)
 
-#------------------ AQUI FUNCIONANDO --------------------------------
+def excluir_departamento(request, id):
+    e = get_object_or_404(models.Departamento, id=id)
+    e.delete()
+    return departamentos(request)
+
+def excluir_instituicao(request, id):
+    e = get_object_or_404(models.Instituicao, id=id)
+    e.delete()
+    return instituicoes(request)
+
+def excluir_projeto_pesquisa(request, id):
+    e = get_object_or_404(models.ProjetoDePesquisa, id=id)
+    e.delete()
+    return projetos_pesquisa(request)
+
+def excluir_curriculo(request, id):
+    e = get_object_or_404(models.Curriculo, id=id)
+    e.delete()
+    return curriculos(request)
+
+#------------------ ATUALIZACAO DE ITENS --------------------------------
 def atualizar_especialidade(request, id):
     e = get_object_or_404(models.Especialidade, id=id)
     if request.method == 'POST':
@@ -281,10 +275,7 @@ def atualizar_especialidade(request, id):
             return especialidades(request)
     else:
         form = forms.EspecialidadeForm(instance=e)
-        print(form)
-        print(request)
         return render(request, 'atualizar_especialidade.html', {'form': form, 'obj': e})
-#---------------------------------------------------------------------------------------
 
 def atualizar_estudante(request, id):
     e = get_object_or_404(models.Estudante, id=id)
@@ -331,3 +322,47 @@ def atualizar_livro(request, id):
     else:
         form = forms.LivroForm(instance=e)
         return render(request, 'atualizar_livro.html', {'form': form, 'obj': e})
+
+def atualizar_departamento(request, id):
+    e = get_object_or_404(models.Departamento, id=id)
+    if request.method == 'POST':
+        form = forms.DepartamentoForm(request.POST, instance=e)
+        if form.is_valid():
+            form.save()
+            return departamentos(request)
+    else:
+        form = forms.DepartamentoForm(instance=e)
+        return render(request, 'atualizar_departamento.html', {'form': form, 'obj': e})
+
+def atualizar_instituicao(request, id):
+    e = get_object_or_404(models.Instituicao, id=id)
+    if request.method == 'POST':
+        form = forms.InstituicaoForm(request.POST, instance=e)
+        if form.is_valid():
+            form.save()
+            return instituicoes(request)
+    else:
+        form = forms.EspecialidadeForm(instance=e)
+        return render(request, 'atualizar_instituicao.html', {'form': form, 'obj': e})
+
+def atualizar_projeto_pesquisa(request, id):
+    e = get_object_or_404(models.ProjetoDePesquisa, id=id)
+    if request.method == 'POST':
+        form = forms.ProjetoPesquisaForm(request.POST, instance=e)
+        if form.is_valid():
+            form.save()
+            return projetos_pesquisa(request)
+    else:
+        form = forms.ProjetoPesquisaForm(instance=e)
+        return render(request, 'atualizar_projeto_pesquisa.html', {'form': form, 'obj': e})
+
+def atualizar_curriculo(request, id):
+    e = get_object_or_404(models.Curriculo, id=id)
+    if request.method == 'POST':
+        form = forms.CurriculoForm(request.POST, instance=e)
+        if form.is_valid():
+            form.save()
+            return curriculos(request)
+    else:
+        form = forms.CurriculoForm(instance=e)
+        return render(request, 'atualizar_curriculo.html', {'form': form, 'obj': e})
